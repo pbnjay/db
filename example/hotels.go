0625318454c031e90c44575d05007c213b6de179
Jeremy Jay <jeremy@pbnjay.com>
2015-02-27 12:02:01 -0500

    add a description to example file

--- example/hotels.go
+++ example/hotels.go
@@ -1,3 +1,25 @@
+// Simple example of package usage. For an example database update and
+// migration on this file, see the commit on this file at:
+// https://github.com/pbnjay/db/commit/7b414cc742aedc13d064fcddd09cb5d16ae05406
+//
+// To see the migration in action, build the prior version of this file and
+// the latest:
+//
+//     $ cd $GOPATH/src/github.com/pbnjay/db/example
+//     $ git checkout 4546a34
+//     $ go build -o hotels_v1 hotels.go
+//     $ git checkout master
+//     $ go build -o hotels_latest hotels.go
+//
+//     $ createdb hotels
+//     $ ./hotels_v1 -db "dbname=hotels" HotelPlace 101
+//     2015/02/27 11:55:59 Creating database
+//     2015/02/27 11:55:59 CREATE TABLE ...
+//     ...
+//     $ ./hotels_latest -db "dbname=hotels" HotelPlace '123 Main St' 202
+//     2015/02/27 11:57:04 Performing database migration from 4 -> 5
+//     2015/02/27 11:57:04 ALTER TABLE hotels ADD COLUMN address varchar;
+//
 package main
 
 import (
